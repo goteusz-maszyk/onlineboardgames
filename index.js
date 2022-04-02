c2ebb688ec6cce9d180e017b06b7239a3db1b769
goteusz-maszyk <mateusz.goszyk@gmail.com>
2022-04-02 19:54:30 +0200

    Game end checking

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ import { createServer } from 'http';
 import { Server } from "socket.io";
 import onCellChange from './lib/cellChange.js';
 import connectionInit from './lib/connectionInit.js';
-process.env.NODE_ENV = "development"
+import checkWin from './lib/winChecker.js';
+
 const app = express()
 const server = createServer(app);
 const io = new Server(server);
 
-const port = process.env.PORT || 5000
+const port = process.argv[2] ? process.argv[2].split(":")[1] : process.env.PORT || 5000
+const path = process.argv[2] ? process.argv[2].split(":")[0] : '127.0.0.1'
 
 let rooms = {}
 
@@ -16,34 +18,44 @@ io.on('connection', (socket) => {
   connectionInit(socket, rooms)
 
   socket.on('cellChange', data => {
+    if (rooms[socket.room].gameEnded) return
     onCellChange(socket, rooms, io, data)
+    checkWin(socket, rooms, io)
   })
 
   setTimeout(() => {
+    if (!rooms[socket.room]) return
     Object.keys(rooms[socket.room].cells).forEach(cell => {
       socket.emit('cellChange', [cell, rooms[socket.room].cells[cell]])
     })
+    if (rooms[socket.room].gameEnded) socket.emit('gameEnd', rooms[socket.room].gameEndData)
   }, 100)
+
 
 
   socket.on('disconnect', () => {
     if (rooms[socket.room]) {
-      rooms[socket.room].connections.splice(rooms[socket.room].connections.indexOf(socket.id), 1)
-      if (rooms[socket.room].connections.length == 0) {
-        delete rooms[socket.room]
-      }
+      delete rooms[socket.room].connections[socket.id]
+      if (Object.keys(rooms[socket.room].connections) == 0) delete rooms[socket.room]
     }
+  })
+
+  socket.on('reset-game', () => {
+    rooms[socket.room].cells = {}
+    rooms[socket.room].gameEnded = false
+    io.to(socket.room).emit('reset-game')
+    for (const i of ['a1', 'b1', 'c1', 'a2', 'b2', 'c2', 'a3', 'b3', 'c3'])
+      io.to(socket.room).emit('cellChange', [i, ""])
   })
 });
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
 
-
 app.get('/', (req, res) => {
-  res.render('tictactoe')
+  res.render('tictactoe', { room: req.query.room })
 })
 
-server.listen(port, () => {
-  console.log(`Online board games server listening on port ${port}`)
+server.listen(port, path, () => {
+  console.log(`Online board games server listening on ${path}:${port}`)
 })
--- public/tictactoe.js
+++ public/tictactoe.js
@@ -3,15 +3,21 @@ const socket = io({
     room: new URLSearchParams(document.location.search).get('room') || ""
   }
 });
-
+let shape
+const cellList = ['a1', 'b1', 'c1', 'a2', 'b2', 'c2', 'a3', 'b3', 'c3']
+const gameStatusDisplay = document.getElementById('game-status')
 document.querySelectorAll('td').forEach((el) => {
   el.addEventListener("click", (e) => {
     socket.emit('cellChange', [e.target.id])
   })
 })
 
+const resetBtn = document.getElementById('reset-game-btn')
+
+resetBtn.style.display = 'none'
 
 socket.on('roomChange', roomID => {
+  document.getElementById("room").innerHTML = "ROOM: " + roomID
   const url = new URL(window.location);
   url.searchParams.set('room', roomID);
   window.history.pushState({}, document.title, url);
@@ -20,3 +26,40 @@ socket.on('roomChange', roomID => {
 socket.on("cellChange", data => {
   document.getElementById(data[0]).innerHTML = data[1]
 });
+
+socket.on("gameEnd", data => {
+  resetBtn.style.display = 'inline'
+  if (!data.winner) {
+    alert("Game ended! It's a draw!")
+    gameStatusDisplay.innerText = "ENDED, DRAW"
+  } else if (data.winner == shape) {
+    alert('You won the game!')
+    gameStatusDisplay.innerText = "ENDED, WIN"
+  } else {
+    alert("Your opponent won the game!")
+    gameStatusDisplay.innerText = "ENDED, DEFEAT"
+  }
+  data.cells.forEach((cell) => {
+    document.getElementById(cell).classList.add("won-cell")
+  })
+})
+
+socket.on('setShape', data => {
+  shape = data
+})
+
+socket.on('disconnect', () => {
+  alert("Server refused/closed connection.\nIt's mostly because of restart or trying to connect to full room")
+})
+
+socket.on('reset-game', () => {
+  resetBtn.style.display = 'none'
+  gameStatusDisplay.innerText = "PENDING"
+  cellList.forEach((cell) => {
+    document.getElementById(cell).classList.remove("won-cell")
+  })
+})
+
+resetBtn.addEventListener('click', () => {
+  socket.emit('reset-game')
+})
